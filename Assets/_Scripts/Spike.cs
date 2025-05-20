8988ccacdfadb2950cf79947728f81fe543e8153
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-05-20 22:43:15 +0200

    Added spike trap

---
+++ Assets/_Scripts/Spike.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Spike : MonoBehaviour
+{
+    public GameObject spikesModel;
+
+    public Vector3 offset = new Vector3(0, 1f, 0);
+    public float popUpDuration = 0.5f;
+    public float returnDuration = 3f;
+    public float waitBeforeReturn = 5f;
+    public int healthToTake = -50;
+
+    private Vector3 initialPosition;
+    private AudioSource audioSource;
+
+    public AudioClip popUpSound;
+    public AudioClip returnSound;
+
+    private bool isUp = false;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isUp)
+        {
+            PopUp();
+        }
+    }
+
+    public void PopUp()
+    {
+        isUp = true;
+        audioSource.PlayOneShot(popUpSound);
+        GameManager.instance.AddHealth(healthToTake);
+
+        LeanTween.move(gameObject, initialPosition + offset, popUpDuration)
+                 .setEase(LeanTweenType.easeOutQuad)
+                 .setOnComplete(() =>
+                 {
+                     // Odczekaj X sekund zanim wrócisz na dó³
+                     LeanTween.delayedCall(gameObject, waitBeforeReturn, ReturnToPosition);
+                 });
+    }
+
+    void ReturnToPosition()
+    {
+        audioSource.PlayOneShot(returnSound);
+        LeanTween.move(gameObject, initialPosition, returnDuration)
+                 .setEase(LeanTweenType.easeInQuad)
+                 .setOnComplete(() =>
+                 {
+                     isUp = false;
+                 });
+    }
+}
